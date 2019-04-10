6299e8961adce63b0ec3cfe2adcccd275f5751b3
mirsafari <miroslav.safaric@gmail.com>
2019-04-10 16:57:41 +0200

    Update mrt-agent.sh

--- mrt-agent.sh
+++ mrt-agent.sh
@@ -1,19 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-PING_COUNT=$1
-TARGET_IP=$2
-MONITOR_NAME=$3
-LOGSTASH_IP=$4
-LOGSTASH_PORT=$5
-MONITORING_INTERVAL=$6
-
-if [ -z "$1" ] ; then
-        echo "USAGE:"
-        echo "$0 <PING_COUNT> <TARGET_IP>"
-        exit
-fi
-
-while true ; do
-        mtr -r -w -j -n -z -c $PING_COUNT $TARGET_IP | ./trap.py --monitor-name $MONITOR_NAME | nc -w $LOGSTASH_IP $LOGSTASH_PORT;
-        sleep $MONITORING_INTERVAL;
-done
+while true; do mtr -r -w -j -n -c $PING_COUNT $TARGET_IP | /scripts/trap.py --monitor-name $MONITOR_NAME | nc -w 30 $LOGSTASH_IP $LOGSTASH_PORT; sleep $MONITORING_INTERVAL; done
